a44cb03bcd5daadc4de74165d2ce09e39b30e1b6
Win Tongtawee <118707963+NoHaxsJustAsian@users.noreply.github.com>
2024-04-21 18:32:34 -0400

    put logo on login

--- src/Login_Register/Login.tsx
+++ src/Login_Register/Login.tsx
@@ -51,6 +51,7 @@ function AuthScreen() {
                 <main className="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6">
                     <div className="max-w-xl lg:max-w-3xl">
         <div>
+        <img src="/logo-black-horizontal-crop.png"/>
             <form onSubmit={onSubmit} style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
                 {formMode === 'register' && (
                     <>
