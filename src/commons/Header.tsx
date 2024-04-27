76a5cb67905177cea52730bdf4cde527be26e34c
Krishna Singh <70067302+Krishna-coder6111@users.noreply.github.com>
2024-04-27 18:16:42 +0000

    login button fix

--- src/commons/Header.tsx
+++ src/commons/Header.tsx
@@ -69,7 +69,7 @@ function Header() {
                         </Nav>
                     ) : (
                         <Nav>
-                            <NavLink className="nav-item" to="/login">Sign In/Sign Up</NavLink>
+                            <NavLink className="nav-item inline-block rounded border border-indigo-600 bg-indigo-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500" to="/login">Login</NavLink>
                         </Nav>
                     )}
                 </Navbar.Collapse>
