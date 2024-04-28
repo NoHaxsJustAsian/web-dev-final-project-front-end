13d72ca38e00caaf9dd336640ac5b7126bd29e60
Win Tongtawee <118707963+NoHaxsJustAsian@users.noreply.github.com>
2024-04-28 14:35:58 -0400

    add change

--- src/Details/Details.tsx
+++ src/Details/Details.tsx
@@ -404,22 +404,22 @@ function Details() {
                 </div>
             </div>
         </section>
-        {user && userRole !== 'seller' ? (
-    userRole === 'buyer' && (
-        <>
-            <ReviewList postid={postIdNumber} />
-            <ReviewForm postid={postIdNumber} />
-        </>
-    )
-) : (
+        {user && userRole === 'buyer' && (
+    <>
+        <ReviewList postid={postIdNumber} />
+        <ReviewForm postid={postIdNumber} />
+    </>
+)}
+
+{(userRole !== 'buyer' && userRole !== 'seller') && (
     <div className="flex justify-center mt-4">
         <a href="#/login" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
             You must be logged in as a buyer to leave a review.
         </a>
     </div>
 )}
-        </div>
-    );
+</div>
+);
 }
 
 export default Details;
