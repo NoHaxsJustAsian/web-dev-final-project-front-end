a58b2abfca30e8e512730bc5df182450c19bae49
Win Tongtawee <118707963+NoHaxsJustAsian@users.noreply.github.com>
2024-04-28 07:16:26 -0400

    fixed header

--- src/commons/Header.tsx
+++ src/commons/Header.tsx
@@ -72,7 +72,7 @@ function Header() {
             {user && (
                 <>
                     <Link className="mr-5 hover:text-gray-900" to="/profile">Profile</Link>
-                    {role === 'buyer' && <Link className="mr-5 hover:text-gray-900" to="/add-product">Add Product</Link>}
+                    {role === 'seller' && <Link className="mr-5 hover:text-gray-900" to="/add-product">Add Product</Link>}
                     <button 
                     type="button" 
                     className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" 
